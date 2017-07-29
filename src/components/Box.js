f199a7b727f604d207ffa823bab2f7f5ef5f15cf
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-29 14:25:05 +0700

    xong

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 
 export default class Box extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 50 };
+        this.up = this.up.bind(this);
+        this.down = this.down.bind(this);
+    }
+
+    up() {
+        // Loi nho o day
+        this.setState({ value: this.state.value + 1 });
+    }
+
+    down() {
+        this.setState({ value: this.state.value - 1 });
+    }
+
     render() {
         return (
             <div>
-                <p>Value: 100</p>
-                <button>Up</button>
+                <p style={{ color: 'green', fontWeight: 'bold' }}>Value: {this.state.value}</p>
+                <button onClick={this.up}>Up</button>
+                <button onClick={this.down}>Down</button>
             </div>
         );
     }
 }
+
+// re nhanh
