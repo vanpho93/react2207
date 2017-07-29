968d7bc48d2bbf39eb6ff90e8ad81769dd14bb53
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-29 10:47:52 +0700

    ket thuc

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// console.log(require('./b')(4, 5));
-const { add, sub } = require('./b');
+const faker = require('faker');
 
-console.log(add(4, 5));
-console.log(sub(4, 5));
+console.log(faker.internet.email());
