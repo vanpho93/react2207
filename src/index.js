27ea60d3b7089278dde9173eb2bbf65052035442
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-29 11:04:15 +0700

    xong

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<p>bbb</p>, document.getElementById('root'));
+registerServiceWorker();
