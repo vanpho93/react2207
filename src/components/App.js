db1125e3f1ca374b7fd10f636fc5b05724d37cdf
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-29 15:50:31 +0700

    get text

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import Card from './Card';
+import List from './List';
 
 const App = () => (
     <div>
-        <Card>AAA</Card>
-        <Card>
-            <p>Hello</p>
-        </Card>
-        <Card>
-            <h3>Hello</h3>
-        </Card>
+        <List />
     </div>
 );
 
--- src/components/List.js
+++ src/components/List.js
@@ -8,17 +8,37 @@ class NewWord {
     }
 }
 
-const arrWords = [
-    new NewWord('Hello', 'Xin chao'),
-    new NewWord('Good bye!', 'Tam biet'),
-    new NewWord('Moring', 'Buoi sang')
-];
-
 export default class List extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { 
+            arrWords:  [
+                new NewWord('Hello', 'Xin chao'),
+                new NewWord('Good bye!', 'Tam biet'),
+                new NewWord('Moring', 'Buoi sang')
+            ]
+        };
+        this.onAddWord = this.onAddWord.bind(this);
+    }
+
+    onAddWord() {
+        const en = this.refs.txtEn.value;
+        const vn = this.refs.txtVn.value;
+        console.log(en, vn);
+    }
+
     render() {
         return (
             <div>
-                { arrWords.map(e => <Word en={e.en} vn={e.vn} key={e.en} />) }
+                <div style={{ backgroundColor: 'gray', padding: 10, width: 150 }}>
+                    <input type="text" placeholder="Enter English word" ref="txtEn" />
+                    <br /><br />
+                    <input type="text" placeholder="Enter Vietnamese meaning" ref="txtVn" />
+                    <br /><br />
+                    <button onClick={this.onAddWord}>Add</button>
+                </div>
+                { this.state.arrWords.map(e => <Word en={e.en} vn={e.vn} key={e.en} />) }
             </div>
         );
     }
